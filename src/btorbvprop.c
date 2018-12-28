3b5a881a56a94bcea335222325ed66eff55e9076
Aina Niemetz <aina.niemetz@gmail.com>
2018-12-28 12:01:36 -0800

    bvprop: shift: Add constraint for result 0.

--- src/btorbvprop.c
+++ src/btorbvprop.c
@@ -951,6 +951,7 @@ bvprop_shift_aux (BtorMemMgr *mm,
   /* z_[bw] = x_[bw] << y_[log_2 bw]
    *
    * SLL:
+   *   z_[bw] == 0_[bw] && x[LSB:LSB] == 1_[1] = 0_[1]
    *   prev_z = x
    *   for i = 0 to log2_bw - 1:
    *     shift = 1 << i
@@ -958,6 +959,7 @@ bvprop_shift_aux (BtorMemMgr *mm,
    *     prev_z = cur_z
    *
    * SRL:
+   *   z_[bw] == 0_[bw] && x[MSB:MSB] == 1_[1] = 0_[1]
    *   prev_z = x
    *   for i = 0 to log2_bw - 1:
    *     shift = 1 << i
@@ -968,7 +970,11 @@ bvprop_shift_aux (BtorMemMgr *mm,
   uint32_t i, n, bw, log2_bw;
   bool res, progress;
   BtorBitVector *bv;
-  BtorBvDomain *d, *tmp_x, *tmp_y, *tmp_z;
+  BtorBvDomain *d, *tmp_x, *tmp_x_bit, *tmp_y, *tmp_z;
+  BtorBvDomain *tmp_eq_z, *tmp_eq_x_bit, *tmp_zero, *tmp_zero_bw, *tmp_one;
+#ifndef NDEBUG
+  BtorBvDomain *d_zero, *d_zero_bw, *d_one;
+#endif
   BtorBvDomainPtrStack d_c_stack, d_shift_stack, d_ite_stack;
   BtorBvDomain **tmp_c, **tmp_shift, **tmp_ite, **tmp_z_prev;
   BtorBvDomain *tmp_res_c;
@@ -1019,6 +1025,34 @@ bvprop_shift_aux (BtorMemMgr *mm,
   tmp_y = btor_bvprop_new (mm, d_y->lo, d_y->hi);
   tmp_z = btor_bvprop_new (mm, d_z->lo, d_z->hi);
 
+  tmp_x_bit = new_domain (mm);
+  tmp_x_bit->lo = btor_bv_uint64_to_bv(mm, btor_bv_get_bit (d_x->lo, 0), 1);
+  tmp_x_bit->hi = btor_bv_uint64_to_bv(mm, btor_bv_get_bit (d_x->hi, 0), 1);
+
+  tmp_eq_z = btor_bvprop_new_init (mm, 1);
+  tmp_eq_x_bit = btor_bvprop_new_init (mm, 1);
+
+  tmp_zero = new_domain (mm);
+  tmp_zero->lo = btor_bv_zero (mm, 1);
+  tmp_zero->hi = btor_bv_zero (mm, 1);
+  tmp_zero_bw = new_domain (mm);
+  tmp_zero_bw->lo = btor_bv_zero (mm, bw);
+  tmp_zero_bw->hi = btor_bv_zero (mm, bw);
+  tmp_one = new_domain (mm);
+  tmp_one->lo = btor_bv_one (mm, 1);
+  tmp_one->hi = btor_bv_one (mm, 1);
+#ifndef NDEBUG
+  d_zero = new_domain (mm);
+  d_zero->lo = btor_bv_zero (mm, 1);
+  d_zero->hi = btor_bv_zero (mm, 1);
+  d_zero_bw = new_domain (mm);
+  d_zero_bw->lo = btor_bv_zero (mm, bw);
+  d_zero_bw->hi = btor_bv_zero (mm, bw);
+  d_one = new_domain (mm);
+  d_one->lo = btor_bv_one (mm, 1);
+  d_one->hi = btor_bv_one (mm, 1);
+#endif
+
   do
   {
     progress = false;
@@ -1027,23 +1061,91 @@ bvprop_shift_aux (BtorMemMgr *mm,
     {
       /**
        * SLL:
+       *   z_[bw] == 0_[bw] && x[LSB:LSB] == 1_[1] = 0_[1]
        *   prev_z = x
        *   for i = 0 to log2_bw - 1:
        *     cur_z = ite (y[i:i], prev_z << shift, prev_z)
        *     prev_z = cur_z
        *
        * SRL:
+       *   z_[bw] == 0_[bw] && x[MSB:MSB] == 1_[1] = 0_[1]
        *   prev_z = x
        *   for i = 0 to log2_bw - 1:
        *     cur_z = ite (y[i:i], prev_z << shift, prev_z)
        *     prev_z = cur_z
        */
 
+      /**
+       * SLL: x_bit = x[LSB:LSB]
+       * SRL: x_bit = x[MSB:MSB]
+       */
+      if (!(res = decomp_step_slice (mm,
+                                     &tmp_x,
+                                     &tmp_x_bit,
+                                     is_srl ? bw - 1 : 0,
+                                     is_srl ? bw - 1 : 0,
+                                     res_d_x,
+                                     res_d_z,
+                                     btor_bvprop_slice,
+                                     &progress)))
+      {
+        goto DONE;
+      }
+
+      /* eq_z = z == 0 */
+      if (!(res = decomp_step_binary (mm,
+                                      &tmp_z,
+                                      &tmp_zero_bw,
+                                      &tmp_eq_z,
+                                      res_d_x,
+                                      res_d_y,
+                                      res_d_z,
+                                      btor_bvprop_eq,
+                                      &progress)))
+      {
+        goto DONE;
+      }
+      assert (!btor_bv_compare (tmp_zero_bw->lo, d_zero_bw->lo));
+      assert (!btor_bv_compare (tmp_zero_bw->hi, d_zero_bw->hi));
+
+      /* eq_x_bit = x_bit == 1 */
+      if (!(res = decomp_step_binary (mm,
+                                      &tmp_x_bit,
+                                      &tmp_one,
+                                      &tmp_eq_x_bit,
+                                      res_d_x,
+                                      res_d_y,
+                                      res_d_z,
+                                      btor_bvprop_eq,
+                                      &progress)))
+      {
+        goto DONE;
+      }
+      assert (!btor_bv_compare (tmp_one->lo, d_one->lo));
+      assert (!btor_bv_compare (tmp_one->hi, d_one->hi));
+
+      /* 0 = eq_z && eq_x_bit */
+      if (!(res = decomp_step_binary (mm,
+                                      &tmp_eq_z,
+                                      &tmp_eq_x_bit,
+                                      &tmp_zero,
+                                      res_d_x,
+                                      res_d_y,
+                                      res_d_z,
+                                      btor_bvprop_and,
+                                      &progress)))
+      {
+        goto DONE;
+      }
+      assert (!btor_bv_compare (tmp_zero->lo, d_zero->lo));
+      assert (!btor_bv_compare (tmp_zero->hi, d_zero->hi));
+
       /* shift = 1 << i */
       bv = BTOR_PEEK_STACK (shift_stack, i);
 
       tmp_shift  = &d_shift_stack.start[i];
       tmp_z_prev = i ? &d_ite_stack.start[i - 1] : &tmp_x;
+
       /**
        * SLL: prev_z << shift
        * SRL: prev_z >> shift
@@ -1201,6 +1303,14 @@ DONE:
   BTOR_RELEASE_STACK (d_shift_stack);
   BTOR_RELEASE_STACK (d_ite_stack);
   BTOR_RELEASE_STACK (shift_stack);
+  btor_bvprop_free (mm, tmp_zero);
+  btor_bvprop_free (mm, tmp_zero_bw);
+  btor_bvprop_free (mm, tmp_one);
+#ifndef NDEBUG
+  btor_bvprop_free (mm, d_zero);
+  btor_bvprop_free (mm, d_zero_bw);
+  btor_bvprop_free (mm, d_one);
+#endif
 
   return res;
 }
